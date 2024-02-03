5950d2f1b934fe476b4dea93b34ebae7962ca5ec
jiyuuuuu <n_jinkaku_y@yahoo.co.jp>
2024-02-03 16:03:20 +0900

    fix:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
           <h2>
             Templates <span>-&gt;</span>
           </h2>
-          <p>Explore starter templates for Next.js.</p>
+          <p>テストです</p>
         </a>
 
         <a
